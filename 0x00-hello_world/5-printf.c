654f913988cc47b3f7e1ec88ec7c3bf3f6e6d519
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-03-10 18:19:13 +0300

    int

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
  * but the outcome is a piece of art,
  * return: 0 if true, otherwise return non-zero
  */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
