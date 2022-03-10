5758cd5fb86bdad7cd83ff9a9d3daafcaef1673e
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-03-10 18:11:21 +0300

    Abbreviations

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main- should print: "with proper grammar, but the outcome is a piece of art,"
+ * main- should print: with proper grammar, 
+ * but the outcome is a piece of art,
  * return: 0 if true, otherwise return non-zero
  */
 {
