4c5529df5c1b59a611fad7e41c7b8be0c5624be5
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-03-10 18:07:28 +0300

    making changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - printing "Programming is like building a multilingual puzzle"
- * return: 0 if correct otherwise return: non-zero
+ * main - printing "Programming is like building a multilingual puzzle
+ * Return: 0 if correct otherwise return: non-zero
  */
 int main(void)
 {
