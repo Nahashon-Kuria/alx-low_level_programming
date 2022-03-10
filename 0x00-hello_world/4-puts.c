5d6d32729f6bdb2a1cf7f1fede5a2581b1139499
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-03-10 17:56:08 +0300

    Executable C-files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing "Programming is like building a multilingual puzzle"
+ * return: 0 if correct otherwise return: non-zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main- should print: "with proper grammar, but the outcome is a piece of art,"
+ * return: 0 if true, otherwise return non-zero
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
