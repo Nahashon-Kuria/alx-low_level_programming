79fa50263ca17dd5cc807db81f9c0fecee0b90d0
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-03-23 21:24:36 +0300

    adding changes

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,58 @@
+#include "holberton.h"
 #include <stdio.h>
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+	char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+	char r[100];
+	char r2[10];
+	char r3[11];
+	char *res;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-	*(p+5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
+	res = infinite_add(n, m, r, 100);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "1234567890";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n= "999999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+	printf("%s + %s = %s\n", n, m, res);
+	}
+	res = infinite_add(n, m, r3, 11);
+	if (res == 0)
+	{
+      printf("Error\n");
+    }
+  else
+    {
+      printf("%s + %s = %s\n", n, m, res);
+    }
   return (0);
 }
