257aa72b3131d0c19bb33aa4d6af7ef5eb991253
Ghost Agent 3 <nahashkuria97@gmail.com>
2022-04-01 12:26:53 +0300

    a program that prints the number of arguments passed into it.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[] __attribute__((unused)))
+ * main - prints the number of arguenments passed to it
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
 {
+/* to suppress unused variable warning */
+	argv = argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
